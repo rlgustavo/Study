46d6bf4ce09d431d56d6f206ddac0507fa3638a9
Gustavo R Lima <rlgustavo1927@gmail.com>
2024-07-13 17:22:54 -0300

    feito

---
+++ Web/Origamid/JavaScriptCompleto/variaveis/app.js
@@ -0,0 +1,89 @@
+// // // Declarar uma variável com o seu nome
+// // var nome = 'Gustavo Lima';
+
+// // // Declarar uma variável com a sua idade
+// // var myAge = 22;
+
+// // // Declarar uma variável com a sua comida
+// // // favorita e não atribuir valor
+// // let myFavoriteFood;
+
+// // // Atribuir valor a sua comida favorita
+// // myFavoriteFood = 'Macarrão'
+
+// // // Declarar 5 variáveis diferentes sem valores
+// // let cidade, estado, pais, endereco, bairro;
+
+// //AULA NUMERO 2
+// // var idade = 28;
+
+// // var simbolo = Symbol();
+// // console.log(typeof simbolo);
+
+// // var nome = 'Gustavo'
+// // var sobrenome = 'Lima'
+// // var nomeCompleto = `${nome} ${sobrenome}`;
+
+// // console.log(nomeCompleto + "tem " + 18 + " de idade");
+// // console.log(`${nomeCompleto} tem ${18} anos de idade`)
+
+// //EXERCICIOS PARA AULA 2
+
+// // Declare uma variável contendo uma string
+// var coisaAleatoria = `Hello World`
+
+// // Declare uma variável contendo um número dentro de uma string
+// var numeroAleatorio = 22;
+
+// // Declare uma variável com a sua idade
+// var idade = 50;
+
+// // Declare duas variáveis, uma com seu nome
+// // e outra com seu sobrenome e some as mesmas
+// var nome            = `Gustavo`;
+// var sobreNome       = `Lima`;
+// var nomeCompleto    = `${nome} ${sobreNome}`;
+
+
+// // Coloque a seguinte frase em uma variável: It's time
+// var fraseSolicitada = `It's Time`
+
+// // Verifique o tipo da variável que contém o seu nome
+// console.log(typeof nome);
+// console.log(nomeCompleto);
+
+
+// AULA 3 exercicios de matematica
+
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20;
+//resultado da expressão acima é 35
+
+
+// Crie duas expressões que retornem NaN
+var primeroNAN = 15 + "Idade";
+var segundoNAN = "20s" * 2
+
+// Somar a string '200' com o número 50 e retornar 250
+var soma = +'200' + 50;
+
+
+// Incremente o número 5 e retorne o seu valor incrementado
+var numero2 = 5
+numero2++
+
+// Como dividir o peso por 2?
+var numero = '80';
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+var pesoPorDois = numero / 2; // NaN (Not a Number)
+
+
+
+
+
+
+
+
+
+
