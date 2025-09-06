0aac7989c0cdd2e72ecc231de19a5a991d9a8971
Gustavo R Lima <rlgustavo1927@gmail.com>
2025-09-06 10:10:15 -0300

    Mudanças

---
+++ Unity/Meu Primeiro Projeto/Assets/Scripts/Tester.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tester : MonoBehaviour
+{
+    // type what ever you want
+
+    /* Introduction to Variables
+    public float power = 2.5f;
+    public double health = 3.5;
+    public int score = 44;
+    private string playerName = "Wizard";
+    public bool isAlive = false;
+    */
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int a = 5;
+        int b = 6;
+
+        int sum = a + b;
+
+        Debug.Log(sum);
+
+        print("The sum of A and B is " + sum);
+
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
